876f2f4951be1bdc3281224fc3cb86b188b65a65
crose1121 <coreycoding1121@outlook.com>
2022-01-18 11:23:55 -0500

    initial commit

---
+++ stringToWord.js
@@ -0,0 +1,33 @@
+function strToWordArr(str){
+    let result = [];
+
+    let word = '';
+    for(let i = 0; i<str.length; i++){
+        //if the current value is an empty space
+        if(str[i] === ' '){
+            if(word.length>0){
+                result.push(word)
+            }
+            word = '';
+        }else{
+            word += str[i]
+        }
+    }
+
+    if(word.length> 0){
+        result.push(word)
+    }
+
+    return result;
+
+}
+
+
+
+console.log(strToWordArr("  Welcome  to week    two!"    )) //["Welcome", "to", "week", "two!"]
+
+/*
+word= 'two!'
+result = ['Welcome', 'to', 'week',  ]
+
+*/
